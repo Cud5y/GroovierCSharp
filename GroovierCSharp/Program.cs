cb2a1b3383116cc2cf6c153ecdc182e52a5ef322
cud5y <jacob@cudsy.ninja>
2024-11-15 15:18:34 +0000

    Initial Commit
    Signed-off-by: cud5y <jacob@cudsy.ninja>

---
+++ GroovierCSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using DSharpPlus;
+
+namespace GroovierCSharp;
+
+internal static class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+        var config = new DiscordConfiguration
+        {
+            Token = token,
+            TokenType = TokenType.Bot,
+            AutoReconnect = true,
+            Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+        };
+
+        var client = new DiscordClient(config);
+
+        client.MessageCreated += async (s, e) =>
+        {
+            if (e.Message.Content.ToLower() == "ping") await e.Message.RespondAsync("Pong!");
+        };
+
+        await client.ConnectAsync();
+        await Task.Delay(-1);
+    }
+}

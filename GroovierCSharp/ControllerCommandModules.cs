0ddf240e6302bac02b6760152bc14ba4673b6870
cud5y <jacob@cudsy.ninja>
2024-11-15 19:30:19 +0000

    Added changes to help prevent exceptions
    Signed-off-by: cud5y <jacob@cudsy.ninja>

--- GroovierCSharp/ControllerCommandModules.cs
+++ GroovierCSharp/ControllerCommandModules.cs
@@ -17,12 +17,18 @@ public class ControllerCommandModules : ApplicationCommandModule
         try
         {
             var vnext = ctx.Client.GetLavalink();
+            if (vnext is null)
+            {
+                await ctx.CreateResponseAsync("Lavalink is not connected.");
+                return;
+            }
+
             var node = vnext.ConnectedNodes.Values.First();
             var channel = ctx.Member.VoiceState.Channel;
             await node.ConnectAsync(channel);
             await ctx.CreateResponseAsync($"Joined {channel.Name}");
         }
-        catch (Exception e)
+        catch (Exception)
         {
             await ctx.CreateResponseAsync("You need to be in a voice channel.");
         }
@@ -32,8 +38,26 @@ public class ControllerCommandModules : ApplicationCommandModule
     public async Task leave(InteractionContext ctx)
     {
         var vnext = ctx.Client.GetLavalink();
+        if (vnext is null)
+        {
+            await ctx.CreateResponseAsync("Lavalink is not connected.");
+            return;
+        }
+
         var node = vnext.ConnectedNodes.Values.First();
+        if (node is null)
+        {
+            await ctx.CreateResponseAsync("Not connection nodes found.");
+            return;
+        }
+
         var connection = node.GetGuildConnection(ctx.Guild);
+        if (connection is null)
+        {
+            await ctx.CreateResponseAsync("No active connection found.");
+            return;
+        }
+
         await connection.DisconnectAsync();
         await ctx.CreateResponseAsync($"Left {connection.Channel.Name}");
     }
